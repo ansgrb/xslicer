3d7663dfc6105ee466aacc1a8d9f7e90d90aa2b2
ansgrb <ansgrb@gmail.com>
2025-01-05 06:56:13 +0200

    Adds append mode for tweet output and removes unused file
    Introduces an option to append tweets to the output file instead of overwriting it
    Deletes an obsolete text file
    Adds separators between tweets in the output file for better readability

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ func main() {
 	textPtr := flag.String("text", "", "The text to split into tweets")
 	filePtr := flag.String("file", "", "Path to a text file containing the input")
 	outputPtr := flag.String("output", "", "Path to save the output tweets (e.g., output.txt)")
+	appendPtr := flag.Bool("append", false, "Append tweets to the output file instead of overwriting it")
 	maxLengthPtr := flag.Int("maxlength", 280, "Maximum length of each tweet")
 	flag.Parse()
 
@@ -50,7 +51,7 @@ func main() {
 
 	// Save tweets to output file if -output flag is provided
 	if *outputPtr != "" {
-		err := saveTweetsToFile(tweets, *outputPtr)
+		err := saveTweetsToFile(tweets, *outputPtr, *appendPtr)
 		if err != nil {
 			log.Fatalf("Error saving tweets to file: %v", err)
 		}
@@ -59,17 +60,25 @@ func main() {
 }
 
 // saveTweetsToFile writes the tweets to a file.
-func saveTweetsToFile(tweets []string, filePath string) error {
-	// Create or truncate the file
-	file, err := os.Create(filePath)
+func saveTweetsToFile(tweets []string, filePath string, append bool) error {
+	// Open the file in the appropriate mode
+	var file *os.File
+	var err error
+	if append {
+		// Append to the file if it exists, or create it if it doesn't
+		file, err = os.OpenFile(filePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	} else {
+		// Create or truncate the file
+		file, err = os.Create(filePath)
+	}
 	if err != nil {
 		return err
 	}
 	defer file.Close()
 
-	// Write each tweet to the file
+	// Write each tweet to the file with a separator
 	for _, tweet := range tweets {
-		_, err := file.WriteString(tweet + "\n")
+		_, err := file.WriteString(tweet + "\n---\n")
 		if err != nil {
 			return err
 		}
